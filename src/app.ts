59eddb2d9833032960378958a828681c90676f85
mylenavendramini <myvendramini@gmail.com>
2022-11-12 14:02:02 +0000

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import mongoose from "mongoose";
 import questionRoutes from "./routes";
 import cors from "cors";
+import * as dotenv from "dotenv";
 
 const app: Express = express();
 const PORT: string | number = process.env.PORT || 4001;
@@ -13,7 +14,10 @@ app.use(cors());
 // Routes:
 app.use(questionRoutes);
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.pihxa.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+// const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.pihxa.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+dotenv.config();
+const uri: any = process.env.DB_STRING;
+console.log(uri);
 
 mongoose
   .connect(uri)
